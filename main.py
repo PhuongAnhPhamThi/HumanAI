4815e0ac27c27ec0c4afbf4f8288a25fd522023d
Konrad Wolf <konrad0701@web.de>
2024-05-12 11:07:34 +0200

    GUI Optical Update and Wait UI
    Tkinter --> Customtkinter
    start_wait_ui() mit neuem Thread hinzugefügt

--- main.py
+++ main.py
@@ -5,11 +5,30 @@ from metagpt.team import Team
 from Roles.Autor import Autor
 from Roles.Editor import Editor
 from Roles.Human_User import Human_User
-from ui.main_ui import start_ui  # import start_ui aus ui um UI zu starten
+from ui.main_ui import start_ui, start_second_ui, start_wait_ui, html_generated  # import start_ui aus ui um UI zu starten
 from runport import start_server
 from workspace.layouterstellen import update_html_from_json
 from workspace.writeLatex import generate_pdf
+import threading
 
+# help function for wait UI
+def controlled_start_wait_ui(stop_event):
+    start_wait_ui(stop_event)
+
+
+# new thread for wait UI
+stop_event = threading.Event()
+wait_thread = threading.Thread(target=controlled_start_wait_ui, args=(stop_event,))
+"""Thread and Wait UI can be started with:
+
+wait_thread.start()
+
+ans should be stopped with:
+
+stop_event.set()
+html_generated()
+thread.join()
+"""
 
 ui_prompt = start_ui()
 
--- ui/main_ui.py
+++ ui/main_ui.py
@@ -1,7 +1,8 @@
-import tkinter as tk
+import customtkinter as ctk
 from tkinter import ttk
 import json
 import webbrowser
+import os
 #from workspace.verbindung import set_link_coverEbook
 
 # note to Konrad: in the main function start_second_ui(prompt) i give prompt directly through parameter "prompt". you
@@ -14,14 +15,22 @@ def some_function(prompt):
     cover_prompt = prompt_coverEbook
 """
 
+ctk.set_appearance_mode("Dark")
+ctk.set_default_color_theme("blue")
+
+geometry = "640x400"
 
 def submit_prompt():
     global ui_prompt  # Use global to modify the global variables
     ui_genre = genre_combobox.get()  # Get the selected genre from the combobox
     ui_gattung = gattung_combobox.get()  # Get the selected gattung from the combobox
+    ui_tonali = tonali_combobox.get()
+    ui_kapitel = kapitel_combobox.get()
     ui_prompt = {
         "genre": ui_genre,
-        "gattung": ui_gattung
+        "gattung": ui_gattung,
+        "tonali": ui_tonali,
+        "kapitel": ui_kapitel
     }
     ui_prompt = json.dumps(ui_prompt, ensure_ascii=False, indent=2)
     root.quit()  # Stop the mainloop
@@ -30,9 +39,9 @@ def submit_prompt():
 
 def copy_to_clipboard(label):
     try:
-        label.config(state="normal")
+        label.configure(state="normal")
         text = label.get("1.0", "end-1c")
-        label.config(state="disabled")
+        label.configure(state="disabled")
         # Die Zwischenablage löschen und den neuen Text einfügen
         second_root.clipboard_clear()
         second_root.clipboard_append(text)
@@ -51,32 +60,54 @@ def save_embed_link():
     second_root.destroy()
 """
 
+def html_generated():
+    wait_label.configure(text="Das E-Book ist fast fertig, klicken Sie auf \"Weiter\" um fortzufahren.")
+    continue_button = ctk.CTkButton(wait_root, text="Weiter", command=wait_root.destroy)
+    continue_button.pack()
+
 
 def start_ui():  # fur User Input am Anfang
-    global root, genre_combobox, gattung_combobox
-    root = tk.Tk()
+    global root, genre_combobox, gattung_combobox, tonali_combobox, kapitel_combobox
+    root = ctk.CTk()
     root.title("E-Book Generator")
+    root.geometry(geometry)
 
     # Genre selection
-    genre_label = tk.Label(root, text="Entscheide dich für ein Genre:")
+    genre_label = ctk.CTkLabel(root, text="Entscheide dich für ein Genre:")
     genre_label.grid(row=0, column=0)
 
-    genres = ["Komödie", "Fantasy", "Thriller", "Horror", "Liebesroman", "Krimi", "Science Fiction", "Steampunk",
-              "Gesellschaftsroman"]
-    genre_combobox = ttk.Combobox(root, values=genres)
+    genres = ["Fantasy", "Thriller", "Horror", "Liebesroman", "Krimi", "Utopie", "Western", "Kinderbuch",
+              "Science Fiction", "Steampunk", "Gesellschaftsroman"]
+    genre_combobox = ctk.CTkComboBox(root, values=genres)
     genre_combobox.grid(row=0, column=1)
 
     # Gattung selection
-    gattung_label = tk.Label(root, text="Wähle eine Gattung:")
+    gattung_label = ctk.CTkLabel(root, text="Wähle eine Gattung:")
     gattung_label.grid(row=1, column=0)
 
     gattungen = ["Prosatext", "Gedicht", "Theaterstück"]
-    gattung_combobox = ttk.Combobox(root, values=gattungen)
+    gattung_combobox = ctk.CTkComboBox(root, values=gattungen)
     gattung_combobox.grid(row=1, column=1)
 
+    # Tonalität selection
+    tonali_label = ctk.CTkLabel(root, text="Lege die Tonalität fest:")
+    tonali_label.grid(row=2, column=0)
+
+    tonalis = ["neutrat", "satirisch", "humorvoll", "kindisch", "melancholisch", "mystisch", "spannend", "romantisch"]
+    tonali_combobox = ctk.CTkComboBox(root, values=tonalis)
+    tonali_combobox.grid(row=2, column=1)
+
+    # Anzahl Kapitel selection
+    kapitel_label = ctk.CTkLabel(root, text="Anzahl der Kapitel:")
+    kapitel_label.grid(row=3, column=0)
+
+    kapitel = ["3", "4", "5", "6", "7", "8", "9"]
+    kapitel_combobox = ctk.CTkComboBox(root, values=kapitel)
+    kapitel_combobox.grid(row=3, column=1)
+
     # Generate button
-    generate_button = tk.Button(root, text="E-Book generieren", command=submit_prompt)
-    generate_button.grid(row=2, column=1)
+    generate_button = ctk.CTkButton(root, text="E-Book generieren", command=submit_prompt)
+    generate_button.grid(row=4, column=1)
 
     root.mainloop()
     return ui_prompt  # Return the selections after the UI has closed
@@ -84,8 +115,9 @@ def start_ui():  # fur User Input am Anfang
 
 def start_second_ui(prompt):  # für book cover
     global second_root, cover_link_entry, saved_link
-    second_root = tk.Tk()
+    second_root = ctk.CTk()
     second_root.title("E-Book Generator")
+    second_root.geometry(geometry)
 
     saved_link = None  # Initialize saved_link variable
 
@@ -94,47 +126,59 @@ def start_second_ui(prompt):  # für book cover
         saved_link = cover_link_entry.get()  # Get the value of cover_link_entry
         second_root.destroy()  # Close the UI
 
-    heading = tk.Frame(second_root, width=400)
-    heading.pack()
-    heading_label = tk.Label(heading, text="Nutze diesen Prompt um ein Cover für das E-Book zu erstellen.")
-    heading_label.pack()
-    cover = tk.Frame(second_root, width=400)
+    heading = ctk.CTkFrame(second_root, width=400)
+    heading.pack(anchor="w")
+    heading_label = ctk.CTkLabel(heading, text="Nutze diesen Prompt um ein Cover für das E-Book zu erstellen.")
+    heading_label.pack(anchor="w")
+    cover = ctk.CTkFrame(second_root, width=400)
     cover.pack(anchor="w")
-    cover_link = tk.Frame(second_root, width=400)
+    cover_link = ctk.CTkFrame(second_root, width=400)
     cover_link.pack()
-    cover_link_label = tk.Label(cover_link,
+    cover_link_label = ctk.CTkLabel(cover_link,
                                 text="Kopiere jetzt den Embed-Code HTML (Vollansicht-Link) und füge ihn hier ein:")
     cover_link_label.grid(row=0, column=0)
-    cover_link_entry = tk.Entry(cover_link, width=67)
+    cover_link_entry = ctk.CTkEntry(cover_link, width=500)
     cover_link_entry.grid(row=1, column=0, sticky="w")
-    cover_link_button = tk.Button(cover_link, text="Link speichern", command=save_embed_link)
+    cover_link_button = ctk.CTkButton(cover_link, text="Link speichern", command=save_embed_link)
     cover_link_button.grid(row=1, column=1)
 
     # Text mit dem Cover Prompt
-    cover_prompt_label = tk.Text(cover, wrap="word", height=8, width=50)
+    cover_prompt_label = ctk.CTkTextbox(cover, wrap="word", height=160, width=500)
     cover_prompt_label.grid(row=0, column=0, sticky="w")
-    cover_prompt_label.insert(tk.END, prompt)
-    cover_prompt_label.config(state="disabled")
+    cover_prompt_label.insert(ctk.END, prompt)
+    cover_prompt_label.configure(state="disabled")
 
     # Button zum Kopieren des Texts
-    copy_button = tk.Button(cover, text="Kopieren", command=lambda: copy_to_clipboard(cover_prompt_label))
+    copy_button = ctk.CTkButton(cover, text="Kopieren", command=lambda: copy_to_clipboard(cover_prompt_label))
     copy_button.grid(row=0, column=1, sticky="se")
 
     # Weiterleitung zu Bing Copilot
-    copilot_label = tk.Label(cover, text="Wir empfehlen dafür den Bing Copilot zu verwenden.")
+    copilot_label = ctk.CTkLabel(cover, text="Wir empfehlen dafür den Bing Copilot zu verwenden.")
     copilot_label.grid(row=1, column=0)
-    copilot_button = tk.Button(cover, text="Zu Copilot", command=lambda: open_url("https://www.bing.com/images/create"))
+    copilot_button = ctk.CTkButton(cover, text="Zu Copilot", command=lambda: open_url("https://www.bing.com/images/create"))
     copilot_button.grid(row=1, column=1)
 
     # Weiterleitung zu Imgbb
-    imgbb_label = tk.Label(cover, text="Lade das online hoch, wir empfehlen imgbb")
+    imgbb_label = ctk.CTkLabel(cover, text="Lade das online hoch, wir empfehlen imgbb")
     imgbb_label.grid(row=2, column=0)
-    imgbb_button = tk.Button(cover, text="Zu imgbb", command=lambda: open_url("https://de.imgbb.com/"))
+    imgbb_button = ctk.CTkButton(cover, text="Zu imgbb", command=lambda: open_url("https://de.imgbb.com/"))
     imgbb_button.grid(row=2, column=1)
 
     second_root.mainloop()
 
     return saved_link
+
+def start_wait_ui(stop_event):
+    while not stop_event.is_set():
+        global wait_root, wait_label
+        wait_root = ctk.CTk()  # Verwendet Customtkinter für das zweite Fenster
+        wait_root.title("E-Book Generator")
+        wait_root.geometry(geometry)
+
+        wait_label = ctk.CTkLabel(wait_root, text="Warte, während das E-Book generiert wird.")
+        wait_label.pack()
+
+        wait_root.mainloop()
 
 
 # This allows the module to be imported without immediately running the UI
